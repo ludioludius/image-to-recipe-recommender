c77f4cadae11510f070ea6e40e1e5a37bc001e96
Ayush Bharat <ayshbrt210@gmail.com>
2024-05-11 23:25:28 -0700

    project initialization

---
+++ src/main/java/dev/ayush/imagetoreciperecommender/ImageToRecipeRecommenderApplication.java
@@ -0,0 +1,13 @@
+package dev.ayush.imagetoreciperecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageToRecipeRecommenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageToRecipeRecommenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ayush/imagetoreciperecommender/ImageToRecipeRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayush.imagetoreciperecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageToRecipeRecommenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
