cf76d1429faa27edafb63cb82c320ae2df5a3448
Ayush Bharat <ayshbrt210@gmail.com>
2024-05-22 11:06:47 -0700

    file upload endpoint

--- src/main/java/dev/ayush/imagetoreciperecommender/controller/IngredientDataController.java
+++ src/main/java/dev/ayush/imagetoreciperecommender/controller/IngredientDataController.java
@@ -2,9 +2,10 @@ package dev.ayush.imagetoreciperecommender.controller;
 
 import dev.ayush.imagetoreciperecommender.model.IngredientData;
 import dev.ayush.imagetoreciperecommender.services.ClarifaiClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,8 +22,28 @@ public class IngredientDataController {
     }
 
     // http:
-    @GetMapping
-    public String getIngredientsFromImage() throws IOException {
-        return new IngredientData(apiClient).toString();
+    @PostMapping
+    public ResponseEntity<String> getIngredientsFromImage(@RequestParam("image") MultipartFile imageFile) throws IOException {
+        byte[] imageBytes;
+
+        // return error if empty file uploaded
+        if (imageFile.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No file selected to upload.");
+        }
+
+        try {
+            // Get the file bytes and store them in memory
+            imageBytes = imageFile.getBytes();
+            // call user code to generate objects from image and return objects in the body
+            System.out.println("File size: " + imageBytes.length);
+            return ResponseEntity.status(HttpStatus.OK).body("File uploaded successfully and stored in memory.");
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file.");
+        }
+
+        // return new IngredientData(apiClient).toString();
     }
+
+    // wish list: new method in Ingredient data class that
 }
