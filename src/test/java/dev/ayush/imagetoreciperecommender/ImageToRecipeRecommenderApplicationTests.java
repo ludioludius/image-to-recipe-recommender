ed1b0123b86aeec20be41c7ecffe3834ba1ddf74
Ayush Bharat <ayshbrt210@gmail.com>
2024-05-28 12:37:19 -0700

    fixed failing test

--- src/test/java/dev/ayush/imagetoreciperecommender/ImageToRecipeRecommenderApplicationTests.java
+++ src/test/java/dev/ayush/imagetoreciperecommender/ImageToRecipeRecommenderApplicationTests.java
@@ -2,6 +2,7 @@ package dev.ayush.imagetoreciperecommender;
 
 import dev.ayush.imagetoreciperecommender.controller.IngredientDataController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class ApplicationSmokeTest {
 
-    // implicit autowired (i.e. dependency injection)
+    @Autowired // Explicitly request Spring to inject the bean
     private IngredientDataController ingredientDataController;
 
     // smoke test to ensure spring application context is not null
