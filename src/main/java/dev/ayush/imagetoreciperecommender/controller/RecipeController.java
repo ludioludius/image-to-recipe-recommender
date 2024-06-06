eb39c7710d61b2d0fd43b5729bc6ff58a80e0626
Ayush Bharat <ayshbrt210@gmail.com>
2024-06-06 15:24:54 -0700

    added spoonacular client and tests

---
+++ src/main/java/dev/ayush/imagetoreciperecommender/controller/RecipeController.java
@@ -0,0 +1,54 @@
+package dev.ayush.imagetoreciperecommender.controller;
+
+import dev.ayush.imagetoreciperecommender.model.IngredientData;
+import dev.ayush.imagetoreciperecommender.services.ClarifaiClient;
+import dev.ayush.imagetoreciperecommender.services.SpoonacularClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/recipes")  // http:localhost:8080/recipes
+@CrossOrigin()
+public class RecipeController {
+
+    private final ClarifaiClient clarifaiClient;
+    private final IngredientData ingredientData;
+    private final SpoonacularClient spoonacularClient;
+
+    // Spring framework calls this constructor, uses dependency injection to inject dependencies
+    public RecipeController(ClarifaiClient clarifaiClient, IngredientData ingredientData, SpoonacularClient spoonacularClient) {
+        this.clarifaiClient = clarifaiClient;
+        this.ingredientData = ingredientData;
+        this.spoonacularClient = spoonacularClient;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> getRecipesFromImage(@RequestParam("image") MultipartFile imageFile) throws IOException {
+
+        // return error if empty file uploaded
+        if (imageFile.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No file selected to upload.");
+        }
+
+        // obtain detected ingredients from image file using API
+        try {
+            byte[] imageBytes = imageFile.getBytes();
+            System.out.println("File size: " + imageBytes.length);
+            List<String> detectedIngredients = ingredientData.generateIngredientList(clarifaiClient, imageBytes);
+
+            // TODO: format call to spoonacular API
+            String recipes = spoonacularClient.getRecipesByIngredients(detectedIngredients);
+
+            return ResponseEntity.status(HttpStatus.OK).body(recipes);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("API ERROR"); // TODO: UPDATE THIS ERROR MESSAGE< DOENST REPRESENT APP ERROR
+        }
+    }
+}
---
+++ src/test/java/services/SpoonacularClientTest.java
@@ -0,0 +1,40 @@
+package services;
+
+import dev.ayush.imagetoreciperecommender.model.DetectedObject;
+import dev.ayush.imagetoreciperecommender.services.ClarifaiClient;
+import dev.ayush.imagetoreciperecommender.services.SpoonacularClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpoonacularClientTest {
+
+    private SpoonacularClient spoonacularClient;
+
+    @BeforeEach
+    void setUp() {
+        spoonacularClient = new SpoonacularClient(new RestTemplate());
+    }
+
+    @Test
+    void testAPICall() {
+
+        try{
+            List<String> ingredients = Arrays.asList("Potato", "Carrot");
+            String recipes = spoonacularClient.getRecipesByIngredients(ingredients);
+            assertFalse(recipes.isEmpty());
+            System.out.println(recipes);
+        } catch (Exception e){
+            e.printStackTrace();
+            fail("Error reading the file");
+        }
+    }
+}
